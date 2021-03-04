dbaf8cc7cca9c6fe1b7b82f707b03866b3a74edd
Afonso -aptsharp <aff-on@hotmail.com>
2021-03-04 18:26:12 -0300

    Criando Primeiro Componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
